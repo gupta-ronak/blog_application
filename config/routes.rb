7000cab8479b0b2395211f268171902fc31836e8
Ronak Gupta <ronak.gupta@deeporion.com>
2023-04-26 16:59:51 +0530

    devise gem_integrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :accounts, controllers: {
+    sessions: 'account/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "article#index"
+  get "article/new", to: "article#new"
+  post "/article", to: "article#create"
 end
